4d4bce7365b25dccb302020642231236502209fc
scinart <akubeej@gmail.com>
2017-09-07 19:49:49 +0800

    add type_trait.hpp

---
+++ include/type_trait.hpp
@@ -0,0 +1,10 @@
+#ifndef GITHUB_SCINART_CPPLIB_TYPE_TRAITS_HPP
+#define GITHUB_SCINART_CPPLIB_TYPE_TRAITS_HPP
+
+#include <type_traits>
+// https://stackoverflow.com/questions/27687389/how-does-void-t-work
+// https://www.v2ex.com/t/387904#reply12
+template <typename T, typename=void> struct is_container : std::false_type {};
+template <typename T>                struct is_container <T, std::__void_t< typename T::value_type > > : std::true_type {};
+
+#endif
---
+++ test/test_type_trait.cpp
@@ -0,0 +1,22 @@
+#include "type_trait.hpp"
+#include <vector>
+
+#include <boost/test/unit_test.hpp>
+
+template <typename, typename = void> class F { public: int operator()() {return 0;} };
+template <typename Container>
+class F<Container, std::enable_if_t<is_container<std::remove_reference_t<Container>>::value, void> >
+{
+public:
+    int operator()() { return 1; }
+};
+
+BOOST_AUTO_TEST_SUITE(type_traits_test)
+
+BOOST_AUTO_TEST_CASE(test_gbk_to_utf8)
+{
+    BOOST_CHECK(F<std::vector<int> >()() == 1);
+    BOOST_CHECK(F<int>()() == 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
