1687a4a4448867ca05a157b9aa3dda4a9311f319
scinart <akubeej@gmail.com>
2018-12-25 17:56:30 -0500

    add seed in rand.hpp

--- include/rand.hpp
+++ include/rand.hpp
@@ -17,6 +17,8 @@ public:
     IntType get() {return dis(gen); }
     IntType set_range(IntType a, IntType b) { dis = std::uniform_int_distribution<IntType>(a, b); }
     IntType get(IntType a, IntType b) { return std::uniform_int_distribution<IntType>(a, b)(gen); }
+    template <class Sseq>
+    void seed(Sseq& seed){gen.seed(seed);}
 private:
     std::random_device rd;  // Will be used to obtain a seed for the random number engine
     std::mt19937_64 gen;       // Standard mersenne_twister_engine seeded with rd()
